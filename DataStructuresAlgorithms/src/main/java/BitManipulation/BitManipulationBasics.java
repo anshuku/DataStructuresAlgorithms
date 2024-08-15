d85d975ca7d4b6ce66311c471eb4f2e6b0bb733d
anshukumar <anshukumar@localhost>
2024-08-15 07:09:19 +0530

    Bit theory

--- DataStructuresAlgorithms/src/main/java/BitManipulation/BitManipulationBasics.java
+++ DataStructuresAlgorithms/src/main/java/BitManipulation/BitManipulationBasics.java
@@ -18,13 +18,13 @@ public class BitManipulationBasics {
 
 	public static void main(String[] args) {
 
-//		int a = 5;//101
-//		System.out.println(a >> 1);
-//		System.out.println(a >> 2);
-//		System.out.println(a >> 3);
-//		System.out.println(a >> 32);
-//		System.out.println(a >> 31);
-//		System.out.println();
+		int a = 5;//101
+		System.out.println(a >> 1);
+		System.out.println(a >> 2);
+		System.out.println(a >> 3);
+		System.out.println(a >> 32);
+		System.out.println(a >> 31);
+		System.out.println();
 
 //		int b = -3;//...011 -> ...100 -> ...101 = -2^2+2^0 = -3
 //		System.out.println(b << 1);//1010 = -6
